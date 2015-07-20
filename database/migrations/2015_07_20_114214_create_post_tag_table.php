28e642687b4802b1829f5ab19405d1cb106776bb
Ysmaen <lecointre.patrick@laposte.net>
2015-07-20 12:43:53 +0100

    premier

---
+++ database/migrations/2015_07_20_114214_create_post_tag_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->integer('tag_id')->unsigned()->nullable();
+            $table->integer('post_id')->unsigned()->nullable();
+
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('CASCADE');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('CASCADE');
+            $table->unique(['tag_id', 'post_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('post_tag', function (Blueprint $table) {
+            $table->dropForeign('post_tag_tag_id_foreign');
+            $table->dropForeign('post_tag_post_id_foreign');
+        });
+
+        Schema::drop('post_tag');
+    }
+}
