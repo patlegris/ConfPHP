e064bd7643c3da4d1a81ec6aea7e11103a59119f
Ysmaen <lecointre.patrick@laposte.net>
2015-07-20 12:10:20 +0100

    premier

--- database/migrations/2015_07_20_104710_create_posts_table.php
+++ database/migrations/2015_07_20_104710_create_posts_table.php
@@ -12,9 +12,21 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('ConfPHP', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->string('title', 50);
+            $table->string('excerpt', 500);
+            $table->text('content');
+            $table->dateTime('date_start');
+            $table->dateTime('date_end');
             $table->timestamps();
+            $table->string('slug', 100);
+            $table->string('thumbnail_link', 200);
+            $table->string('url_site', 200);
+            $table->enum('status', ['publish', 'unpublish']);
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('SET NULL');
         });
     }
 
@@ -25,6 +37,11 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('ConfPHP');
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign('posts_user_id_foreign');
+        });
+
+        Schema::drop('posts');
     }
+}
 }
