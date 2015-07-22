4634d83c5f85d41687655e00002376058892c987
Ysmaen <lecointre.patrick@laposte.net>
2015-07-22 11:23:59 +0100

    premier

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -9,5 +9,27 @@ use App\Http\Controllers\Controller;
 
 class BlogController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::published()->get();
+        return view('blog.index', compact('posts'));
+    }
+
+    public function showPostByCategory($id)
+    {
+        $posts = Category::find($id)->posts()->published()->get();
+        return view('blog.category', compact('posts'));
+    }
+
+    public function showPost($id, $slug = '')
+    {
+        $post = Post::published($id)->first();
+        return view('blog.single', compact('post'));
+    }
+
+    public function showTag($id)
+    {
+        $posts = Tag::find($id)->posts;
+        return view('blog.tag', compact('posts'));
+    }
 }
