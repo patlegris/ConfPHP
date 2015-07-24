91121b3857ae2b59544962946d4c954a4161ce0d
Ysmaen <lecointre.patrick@laposte.net>
2015-07-24 16:02:21 +0100

    premier

--- resources/views/front/includes/head.blade.php
+++ resources/views/front/includes/head.blade.php
@@ -1,3 +1,4 @@
+<!doctype html>
 <!--[if lt IE 7]>
 <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
 <!--[if IE 7]>
@@ -6,18 +7,25 @@
 <html class="no-js lt-ie9" lang=""> <![endif]-->
 <!--[if gt IE 8]><!-->
 <html class="no-js" lang=""> <!--<![endif]-->
-<html lang="fr">
-
 <head>
-    <meta charset="UTF-8">
-    <title>
-    </title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title></title>
     <meta content="conférence PHP" name="description">
     <meta content="Antoine AFUP" name="author">
     <meta content="Paris, France" name="geo.placename">
-    <!--[if lt IE 9]>
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
 
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <style>
+        /*body {*/
+        /*padding-top: 50px;*/
+        /*padding-bottom: 20px;*/
+        /*}*/
+    </style>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
-</head>
+
+    <script src="assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
