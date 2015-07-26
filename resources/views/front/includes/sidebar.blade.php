468935f7d66b88a47a0fafb66ad46eaee44a05df
Ysmaen <lecointre.patrick@laposte.net>
2015-07-26 04:48:01 +0100

    deuxième

--- resources/views/front/includes/sidebar.blade.php
+++ resources/views/front/includes/sidebar.blade.php
@@ -1,7 +1,7 @@
 <section id="sidebar">
     <h1>sponsors</h1>
-    <img class="logo" src="assets/upload/logos/elao_logo_150px.png">
-    <img class="logo" src="assets/upload/logos/zol-logo.png">
-    <img class="logo" src="assets/upload/logos/logo-large.png">
-    <img class="logo" src="assets/upload/logos/Elephpant.png">
+    <img class="logo" src="{{asset('assets/upload/logos/elao_logo_150px.png')}}">
+    <img class="logo" src="{{asset('assets/upload/logos/zol-logo.png')}}">
+    <img class="logo" src="{{asset('assets/upload/logos/logo-large.png')}}">
+    <img class="logo" src="{{asset('assets/upload/logos/Elephpant.png')}}">
 </section>
