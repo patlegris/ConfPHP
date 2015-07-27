cbea9b34133c378645b9e3841ff76602c8c0d113
Ysmaen <lecointre.patrick@laposte.net>
2015-07-27 16:34:38 +0100

    deuxième

--- resources/views/front/blog/single.blade.php
+++ resources/views/front/blog/single.blade.php
@@ -2,22 +2,40 @@
 @section('body')
     <div id="main" role="main">
         <section id="post">
-            <h1>Conférence intéressantes autour du PHP</h1>
-            <div class="post">
-                    <h2 class="title-conf"><a href="{{url('single',[$post->id,$post->slug])}}"
-                                              class="link-post">{{$post->title}}</a></h2>
-                    <img class="left" src="assets/upload/{{$post->thumbnail_link}}">
+            <h1>Conférences intéressantes autour du PHP</h1>
 
-                    <p>{{$post->excerpt}}</p>
-                    <br>
-                    <a class="link" href="{{url('single',[$post->id,$post->slug])}}">lire la suite...</a>
-                    <br><br>
-                    <strong>Tags :</strong>
-                    @foreach( $post->tags as $tag)
-                        <a href="{{url('tag/'.$tag['id'])}}">{{$tag->name}}/</a>
-                    @endforeach
-                    <h3 class="date">début: {{$post->date_start}} - fin: {{$post->date_end}}</h3>
+            <div class="post">
+                <h2 class="title-conf"><a href="{{url('conference',[$post->id,$post->slug])}}"
+                                          class="link-post">{{$post->title}}</a></h2>
+                <img class="left" src="{{asset('assets/upload/'.$post->thumbnail_link)}}">
+
+                <p>{{$post->content}}</p>
+                <br><br><br>
+                <strong>Tags :</strong>
+                @foreach( $post->tags as $tag)
+                    <a href="{{url('tag/'.$tag->id)}}">{{$tag->name}}/</a>
+                @endforeach
+                <br><br>
+
+                <h3 class="date">Début: {{$post->date_start}} - Fin: {{$post->date_end}}</h3>
             </div>
+        </section>
+
+
+        <section id="commentaires">
+            <h3 class="comment">Commentaires</h3>
+            @if(count($post->comment)>0)
+                <ul>
+                    @foreach($post->comments as $comment)
+                        <li>{{$comment->email}}</li>
+                        {{$comment->message}}<br/><br/>
+                    @endforeach
+                </ul>
+            @else
+                <p>Il n'y a pas de commentaires sur ce sujet, soyez le premier !</p>
+            @endif
+            {{--</section>--}}
+            {{--<section id="">--}}
         </section>
     </div>
 @endsection
