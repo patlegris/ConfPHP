b415e0fca93c13b5826618e8f77db5bbd8bfce3d
Ysmaen <lecointre.patrick@laposte.net>
2015-07-28 10:29:24 +0100

    Sponsors cliquables

--- resources/views/front/includes/sidebar.blade.php
+++ resources/views/front/includes/sidebar.blade.php
@@ -1,7 +1,15 @@
 <section id="sidebar">
     <h1>sponsors</h1>
-    <img class="logo" src="{{asset('assets/upload/logos/elao_logo_150px.png')}}">
-    <img class="logo" src="{{asset('assets/upload/logos/zol-logo.png')}}">
-    <img class="logo" src="{{asset('assets/upload/logos/logo-large.png')}}">
-    <img class="logo" src="{{asset('assets/upload/logos/Elephpant.png')}}">
+    <a href="http://www.elao.com/fr">
+        <img class="logo" src="{{asset('assets/upload/logos/elao_logo_150px.png')}}">
+    </a>
+    <a href="http://www.zol.fr/">
+        <img class="logo" src="{{asset('assets/upload/logos/zol-logo.png')}}">
+    </a>
+    <a href="http://www.jolicode.com">
+        <img class="logo" src="{{asset('assets/upload/logos/logo-large.png')}}">
+    </a>
+    <a href="http://php.net/">
+        <img class="logo" src="{{asset('assets/upload/logos/Elephpant.png')}}">
+    </a>
 </section>
